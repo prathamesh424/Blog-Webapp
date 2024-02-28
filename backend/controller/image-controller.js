63cb80a48ca61f31eb599a2fbbd974b02a73a26d
Prathamesh Gursal <prathmeshgursal3110@gmail.com>
2024-02-29 00:03:33 +0530

    mongo fix

--- backend/controller/image-controller.js
+++ backend/controller/image-controller.js
@@ -27,8 +27,6 @@ export const uploadImage = (request, response) => {
 export const getImage = async (request, response) => {
     try {   
         const file = await gfs.files.findOne({ filename: request.params.filename });
-        // const readStream = gfs.createReadStream(file.filename);
-        // readStream.pipe(response);
         const readStream = gridfsBucket.openDownloadStream(file._id);
         readStream.pipe(response);
     } catch (error) {
